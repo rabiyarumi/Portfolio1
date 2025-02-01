c3f02178b51dc79813418de101e42f97e3f9e0ed
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-02 03:04:59 +0600

    added initial setup

---
+++ Portfolio1-client/src/Layouts/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1 className='text-5xl font-bold text-pink-600'>Hello world</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
