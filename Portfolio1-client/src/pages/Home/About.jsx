fd1c3bce2596feaf56e421d8db6b431e581c8249
rabiyarumi <rabiyaarumi@gmail.com>
2025-03-07 03:25:18 +0600

    create about me section

---
+++ Portfolio1-client/src/pages/Home/About.jsx
@@ -0,0 +1,37 @@
+import img from "../../assets/codding_girl.jpg";
+
+const About = () => {
+  return (
+    <section className="py-12 px-6 sm:px-12 lg:px-2  w-[90%] lg:w-full mx-auto">
+      <h2 className="text-4xl font-bold text-emerald-500  mb-10 text-center">About Me</h2>
+      <div className="md:flex gap-10">
+        <div className="md:w-1/2">
+          <img src={img} className=" h-full" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
+
+{
+  /* <div className=" flex flex-col  justify-center md:w-1/2">
+          <p className="mb-3  ">
+            Hi, I’m Rabiya Rumi, a passionate learner and a budding software
+            developer with an unquenchable curiosity for technology. I thrive on transforming complex problems into intuitive user interfaces using React
+          </p>
+          <p className="mb-3">
+            My journey into programming is simple yet exciting—I developed an
+            interest in coding and decided to pursue it wholeheartedly. I enjoy
+            implementing new and creative ideas in my projects, constantly
+            exploring the latest technologies to expand my skills.
+          </p>
+          <p className="mb-3">
+            Beyond programming, I have a passion for solving puzzles like Rubik's
+            cubes and indulging in fashion design, where I get to express my
+            creativity in unique ways. These hobbies help me stay inspired and
+            bring a fresh perspective to my coding journey.
+          </p>
+        </div> */
+}
--- Portfolio1-client/src/pages/Home/Hero.jsx
+++ Portfolio1-client/src/pages/Home/Hero.jsx
@@ -2,9 +2,9 @@ import { motion } from "motion/react";
 import profilePic from "../../assets/pic-2.jpg";
 const Hero = () => {
   return (
-    <div className="relative overflow-clip min-h-screen text-white bg-[linear-gradient(to_bottom,#000,#071E18_35%,#208A65_67%,#35FB8E_85%)]">
+    <div className="relative overflow-clip  text-white bg-[linear-gradient(to_bottom,#000,#071E18_35%,#208A65_67%,#35FB8E_85%)]">
       <div className="absolute bg-black w-[2400px] h-[1000px] rounded-[50%] left-1/2 -translate-x-1/2 bg-[radial-gradient(closest-side,#000_85%,#249974)] top-[450px] border-[1px] border-[#8CD6DE]/30" />
-      <div className="container relative mx-auto px-4 pt-12 pb-24">
+      <div className="container relative mx-auto px-4 pt-12 pb-14">
         <div className="flex flex-col items-center justify-center text-center z-10">
           {/* img div */}
           <motion.div
