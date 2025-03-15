5ed125f7099c1dccdf0b4b3b876b3f903021e056
rabiyarumi <rabiyaarumi@gmail.com>
2025-03-15 07:53:08 +0600

    Modify Hero section

--- Portfolio1-client/src/pages/Home/Hero2.jsx
+++ Portfolio1-client/src/pages/Home/Hero2.jsx
@@ -1,93 +1,30 @@
-import { div } from 'framer-motion/client';
-import React from 'react';
+import { div } from "framer-motion/client";
 
 const Hero2 = () => {
-    return (
-      <>
-       <div className='min-h-[300px] bg-[#28443F]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#F2FD7D]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl '>Rabiya Rumi</h1>
-         </div>
-       </div>
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-[url('/your-background.jpg')] bg-cover"></div>
 
-       <div className='min-h-[300px] bg-[#F66435]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#F4EFCA]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#F4EFCA]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#F66435]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
+      <div className=" h-[300px]">
+        <div className="p-6 max-w-sm bg-[#081C15]/40 backdrop-blur-lg border border-white/20 rounded-2xl shadow-lg">
+          <h2 className="text-white text-2xl font-semibold">Glassmorphism</h2>
+          <p
+            className="text-white/80 mt-2 p-2 bg-[#081C15] 
+          62+6222border border-white/20 rounded-2xl shadow-lg"
+          >
+            A beautiful glass effect with Tailwind CSS.
+          </p>
+        </div>
 
-       <div className='min-h-[300px] bg-[#202216]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#F2DE9B]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-
-       <div className='min-h-[300px] bg-[#EFDFBB]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#722F37]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#722F37]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#EFDFBB]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#3E4A56]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#CEC6B3]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#436850]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#FBFADA]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#352208]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#e1bb80]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#233d4d]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#fe7f2d]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#fe7f2d]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#233d4d]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#e07a5f]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#f4f1de]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-       <div className='min-h-[300px] bg-[#00272b]'>
-         <div className='flex justify-center items-center flex-col pt-[90px] text-[#f0ff4f]'>
-            <p>Hi I'm</p>
-            <h1 className='text-6xl'>Rabiya Rumi</h1>
-         </div>
-       </div>
-
-       </>
-    );
+        <div className="p-6 max-w-sm bg-white/10  backdrop-blur-lg border border-white/20 rounded-2xl shadow-lg">
+          <h2 className="text-white text-2xl font-semibold">Glassmorphism</h2>
+          <p className="text-white/80 mt-2">
+            A beautiful glass effect with Tailwind CSS.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero2;
